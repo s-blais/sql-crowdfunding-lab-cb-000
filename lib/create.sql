699225f364384663336f3145ddf855f6d7bffa1f
Stephen Blais <stephen@s-blais.com>
2019-10-04 19:38:38 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user INTEGER,
+  project INTEGER
+);
