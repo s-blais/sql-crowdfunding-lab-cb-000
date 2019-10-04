94a293493cbaaff05677ea78da95ebb5d97aa321
Stephen Blais <stephen@s-blais.com>
2019-10-04 19:59:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,30 +1,30 @@
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project One", "Tech", 50000, "2019-01-01", "2019-02-01");
+VALUES ("Project One", "Tech", 50000, "2019-01-01", "2019-01-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Two", "Home", 50000, "2019-02-01", "2019-03-01");
+VALUES ("Project Two", "Home", 50000, "2019-02-01", "2019-02-28");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Three", "Lifestyle", 50000, "2019-03-01", "2019-04-01");
+VALUES ("Project Three", "Lifestyle", 50000, "2019-03-01", "2019-03-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Four", "Apparel", 50000, "2019-04-01", "2019-05-01");
+VALUES ("Project Four", "Apparel", 50000, "2019-04-01", "2019-04-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Five", "Tech", 50000, "2019-05-01", "2019-06-01");
+VALUES ("Project Five", "Tech", 50000, "2019-05-01", "2019-05-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Six", "Lifestyle", 50000, "2019-06-01", "2019-07-01");
+VALUES ("Project Six", "Lifestyle", 50000, "2019-06-01", "2019-06-30");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Seven", "Home", 50000, "2019-07-01", "2019-08-01");
+VALUES ("Project Seven", "Home", 50000, "2019-07-01", "2019-07-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Eight", "Tech", 50000, "2019-08-01", "2019-09-01");
+VALUES ("Project Eight", "Tech", 50000, "2019-08-01", "2019-08-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Nine", "Tech", 50000, "2019-09-01", "2019-10-01");
+VALUES ("Project Nine", "Tech", 50000, "2019-09-01", "2019-09-31");
 INSERT INTO projects
 (title, category, funding_goal, start_date, end_date)
-VALUES ("Project Ten", "Apparel", 50000, "2019-10-01", "2019-11-01");
+VALUES ("Project Ten", "Apparel", 50000, "2019-10-01", "2019-10-31");
